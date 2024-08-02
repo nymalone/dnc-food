7e605cb94ecf3831c3458485b3707e4aa860cec3
nymalone <malone.nykolle@gmail.com>
2024-08-02 14:31:31 -0300

    ready

--- src/components/ProductModal/index.js
+++ src/components/ProductModal/index.js
@@ -10,8 +10,29 @@ const ProductModal = ({ product, onClose }) => {
         <button className="close-button" onClick={onClose}>
           &times;
         </button>
-        <img src={product.image} alt={product.description} />
-        <h2>{product.description}</h2>
+        <div className="modal-body">
+          <div className="left-side">
+            <div className="product-card-small">
+              <img
+                src={product.image}
+                alt={product.title}
+                className="product-image-small"
+              />
+              <div className="product-details-small">
+                <p>{product.title}</p>
+              </div>
+            </div>
+            <p className="product-details-small">{product.description}</p>
+          </div>
+          <div className="right-side">
+            <h1 className="title">Minha Sacola</h1>
+            <p className="product-title-small">{product.title}</p>
+            <div className="divider"></div>
+            <p className="product-price-small">{product.price}</p>
+            <button className="modal-button">Continuar Comprando</button>
+            <button className="modal-button">Finalizar Compra</button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/utils/mockProducts.js
+++ src/utils/mockProducts.js
@@ -2,32 +2,46 @@ const products = [
   {
     id: 1,
     image: "https://via.placeholder.com/330",
-    description: "X-Burguer",
+    title: "X-Burguer",
+    price: "R$50,00",
+    description:
+      "200gr carne bovina, mussarela, alface e tomata. Acompanha fritas.",
   },
   {
     id: 2,
     image: "https://via.placeholder.com/330",
-    description: "Açai com mix de frutas",
+    title: "Açai com mix de frutas",
+    price: "RS30,00",
+    description: "Bowl açai 500ml.",
   },
   {
     id: 3,
     image: "https://via.placeholder.com/330",
-    description: "Frongo frito crocante",
+    title: "Frango frito crocante",
+    price: "RS45,00",
+    description:
+      "Frango crocante, acompanha batata frita e molhos especiais caseiros.",
   },
   {
     id: 4,
     image: "https://via.placeholder.com/330",
-    description: "Pastel",
+    title: "Pastel",
+    price: "RS20,00",
+    description: "30cm, acompanha fritas.",
   },
   {
     id: 5,
     image: "https://via.placeholder.com/330",
     description: "Pizza",
+    price: "RS80,00",
+    description: "8 pedaços, borda de catupiry.",
   },
   {
     id: 6,
     image: "https://via.placeholder.com/330",
     description: "Biscoito",
+    price: "RS30,00",
+    description: "Amenteigado com Nutella.",
   },
 ];
 
